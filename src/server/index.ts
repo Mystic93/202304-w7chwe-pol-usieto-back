a01a6adf3c0a7f704efa5a04833c40b92b6ea37c
Pol <polgil24@hotmail.com>
2023-05-20 13:26:55 +0200

    Add cors middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,24 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://202304-w7chwe-pol-usieto-front.netlify.app/",
+];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 const app = express();
+
+app.use(cors(options));
+
+app.disable("x-powered-by");
+
+app.use(express.json());
+
+app.use(morgan("dev"));
 
 export default app;
