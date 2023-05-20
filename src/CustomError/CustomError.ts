c01b7da934f9a59cb92eafd785b3745bb18cdef2
Pol <polgil24@hotmail.com>
2023-05-20 13:29:34 +0200

    Add CustomError class

---
+++ src/CustomError/CustomError.ts
@@ -0,0 +1,12 @@
+class CustomError extends Error {
+  constructor(
+    public statusCode: number,
+    message: string,
+    public publicMessage?: string
+  ) {
+    super(message);
+    this.publicMessage = publicMessage ?? message;
+  }
+}
+
+export default CustomError;
